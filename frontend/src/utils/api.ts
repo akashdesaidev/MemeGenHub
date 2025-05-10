d5e5cb9ca45207e575f83d850988fadf508312a2
Akash-QuickTouch <akash.desai@quicktoich.co.in>
2025-05-10 20:59:59 +0530

    added build command

--- frontend/src/utils/api.ts
+++ frontend/src/utils/api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:5000/api";
+const API_URL = "https://meme-gen-hub.vercel.app/api";
 // Flag to prevent redirect loops
 let isRedirecting = false;
 
